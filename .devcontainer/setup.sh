088b8610c32a44551a50aba5bc80b482c4c31480
Quentigus <quentin.gosselin@gmail.com>
2026-09-13 20:42:21 +0200

    Modify React app setup to use temporary folder
    Change React app initialization to use a temporary folder for setup.

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+temporary_folder=tmp
+
 # Vérifie si le projet React est déjà créé (évite de réécrire par-dessus s'il existe déjà)
 if [ ! -f "package.json" ]; then
   echo "🚀 Initialisation de l'application React (Vite)..."
 
   # Crée l'application React dans le dossier courant (.)
-  npm create vite@latest . --yes --- --template=react --overwrite=ignore --eslint=true --no-immediate
+  npm create vite@latest $temporary_folder --yes --- --template=react --eslint=true --no-immediate
+  cp -rn $temporary_folder .
+  rm -rf $temporary_folder
 
   # Installe les dépendances (dont Bootstrap par exemple)
   npm install
