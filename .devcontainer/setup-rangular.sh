89c8920114c77ef27db6e30bd1c339bda129d067
Quentigus <quentin.gosselin@gmail.com>
2026-09-14 18:32:09 +0200

    test angular

---
+++ .devcontainer/setup-rangular.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+temporary_folder=tmp
+
+# Vérifie si le projet Angular est déjà créé (évite de réécrire par-dessus s'il existe déjà)
+if [ ! -f "package.json" ]; then
+  echo "🚀 Initialisation de l'application Angular (Vite)..."
+
+  # Crée l'application Angular dans le dossier courant (.)
+  npm i -g @angular/cli
+  ng new --skip-install --skip-tests
+  # cp -r --update=none $temporary_folder/* .
+  # rm -rf $temporary_folder
+
+  # Installe les dépendances (dont Bootstrap par exemple)
+  # npm install
+  # npm install bootstrap
+
+  echo "✅ Application Angular prête ! Tape 'npm run dev' pour lancer le serveur."
+fi
